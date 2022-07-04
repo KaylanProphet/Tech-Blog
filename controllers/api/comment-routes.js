3fe836cae8347ed089b8e4b9ff6c18fb05665b61
Kaylan Prophet <kaylan.prophet@gmail.com>
2022-07-04 14:44:10 -0400

    reworked GET and DELETE for comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,26 +4,40 @@ const withAuth = require('../../utils/auth');
 
 //GET all comments, if it doesnt work log the error and send it back to user
 router.get('/', (req, res) => {
-  Comment.findAll()
+  Comment.findAll({
+    attributes: [
+      'id',
+      'commentary',
+      'user_id',
+      'post_id',
+      'date_created'
+    ],
+    order: [['date_created', 'DESC']]
+  })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
       console.log(err);
-      res.status(500).json(err)
+      res.status(500).json(err);
     });
 });
 
+
 //POST
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
-    });
-    res.json(newComment);
-  } catch (err) {
-    res.status(500).json(err);
+router.post('/', withAuth, (req, res) => {
+  if (req.session.loggedIn) {
+    Comment.create({
+      commentary: req.body.commentary,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   }
 });
+
 
 // DELETE
 router.delete('/:id', (req, res) => {
